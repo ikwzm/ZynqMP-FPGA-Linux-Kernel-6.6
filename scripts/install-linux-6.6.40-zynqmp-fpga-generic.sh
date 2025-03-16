c0eb8dea5f27a99e89658a66a911f695f68c3d74
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2025-03-16 12:56:28 +0900

    [update] script files for install linux-6.6.40-zynqmp-fpga-generic-2

--- scripts/install-linux-6.6.40-zynqmp-fpga-generic.sh
+++ scripts/install-linux-6.6.40-zynqmp-fpga-generic.sh
@@ -5,7 +5,7 @@ REPO_DIR=$(cd $(dirname $0); cd .. ; pwd)
 KERNEL_VERSION=6.6.40
 EXTRA_VERSION=-zynqmp-fpga
 LOCAL_VERSION=-generic
-BUILD_VERSION=1
+BUILD_VERSION=2
 
 . "$SCRIPT_DIR/install-variables-zynqmp-fpga.sh"
 . "$SCRIPT_DIR/install-command.sh"
