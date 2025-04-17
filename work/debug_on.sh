0252361472f525c9a95f49bc517220ed8c54e1e2
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2025-04-17 14:21:17 +0900

    [add] script and devicetree files for debug to work/

---
+++ work/debug_on.sh
@@ -0,0 +1,6 @@
+echo "file drivers/of/dynamic.c  +p" > /sys/kernel/debug/dynamic_debug/control
+echo "file drivers/of/overlay.c  +p" > /sys/kernel/debug/dynamic_debug/control
+echo "file drivers/of/configfs.c +p" > /sys/kernel/debug/dynamic_debug/control
+echo "file drivers/of/fdt.c      +p" > /sys/kernel/debug/dynamic_debug/control
+echo "file drivers/of/platform.c +p" > /sys/kernel/debug/dynamic_debug/control
+echo "file drivers/base/core.c   +p" > /sys/kernel/debug/dynamic_debug/control
---
+++ work/test.sh
@@ -0,0 +1,4 @@
+dtbo-config -i dummy-ng --dts dummy-ng.dts
+dtbo-config -r dummy-ng
+dtbo-config -i dummy-ok --dts dummy-ok.dts
+dtbo-config -r dummy-ok
